a739b4ba40c776b02c5be30a784b47138936eefc
orginux <orginux@protonmail.com>
2021-09-27 23:58:54 +0300

    script for vars

---
+++ set_vars.sh
@@ -0,0 +1,9 @@
+echo -n "AWS_ACCESS_KEY_ID:"
+read KEY_ID
+echo -n "AWS_SECRET_ACCESS_KEY:"
+read SECRET_ACCESS_KEY
+
+export AWS_ACCESS_KEY_ID=${KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION="eu-west-1"
+bash
