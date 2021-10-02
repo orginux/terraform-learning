df8bbc461daf42a1ac3bb9fb88bd1451b9d640a8
orginux <orginux@protonmail.com>
2021-10-02 22:50:59 +0300

    lesson 02

--- lab-01/main.tf
+++ lab-01/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "first_instace" {
   # Red Hat Enterprise Linux 8
   ami           = "ami-0ec23856b3bad62d3"
   instance_type = "t2.micro"
+  key_name      = "orginux-ssh-key"
 
   tags = {
     Name    = "first_instace"
